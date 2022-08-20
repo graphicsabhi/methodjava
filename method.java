f51fd9646a3e91107a320e39c29ebc1ff82ed80a
Satyam <satyamsobhraj819@gmail.com>
2022-08-21 01:19:26 +0530

    create methods

---
+++ method.java
@@ -0,0 +1,28 @@
+public class method {
+
+    // add two
+    // number using methods
+
+    // static void addNumbers(int a, int b) {
+    //     int sum = a + b;
+    //     System.out.println(sum);
+    // }
+
+    // public static void main(String[] args) {
+    //     addNumbers(2, 3);
+
+    static int addNumbers(int a, int b){
+        int sum;
+        sum=a+b;
+        return sum;
+
+    }
+    public static void main(String[] args) {
+        int result;
+        result =addNumbers(33, 33);
+        System.out.println(result);
+    }
+
+    }
+
+
